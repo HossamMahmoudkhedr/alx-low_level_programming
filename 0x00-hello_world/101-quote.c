39723ced3f4eec5905381a68fedb58b5d9c9e8ff
Hossam Mahmoud <nhem318@gmail.com>
2023-03-17 03:42:31 +0200

    modify quote file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
 
 int main(void)
 {
+
 char *quote = "and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n";
-write(STDOUT_FILENO, quote, strlen(quote));
+write(STDOUT_FILENO, quote, 59);
 return (1);
 }
