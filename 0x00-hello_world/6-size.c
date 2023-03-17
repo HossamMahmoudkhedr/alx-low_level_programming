576ca6eaaf5401e456e6c0c3f85fff0e7d3371fd
Hossam Mahmoud <nhem318@gmail.com>
2023-03-17 03:06:02 +0200

    modify size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void){
+int main(void)
+{
 size_t charSize = sizeof(char);
 size_t intSize = sizeof(int);
 size_t longSize = sizeof(long int);
@@ -13,7 +14,7 @@ size_t longLongSize = sizeof(long long int);
 size_t floatSize = sizeof(float);
 
 printf("Size of a char: %zu byte(s)\n", charSize);
-printf("Size of a int: %zu byte(s)\n", intSize);
+printf("Size of an int: %zu byte(s)\n", intSize);
 printf("Size of a long int: %zu byte(s)\n", longSize);
 printf("Size of a long long int: %zu byte(s)\n", longLongSize);
 printf("Size of a float: %zu byte(s)\n", floatSize);
