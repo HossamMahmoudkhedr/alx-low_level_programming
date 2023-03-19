4f321a1f292ca597c2660b73878db992ac58e21d
Hossam Mahmoud <nhem318@gmail.com>
2023-03-19 11:49:52 +0200

    modify the format of positive_or_negative.c file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
@@ -12,11 +17,11 @@ n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
 printf("%d is negative\n", n);
-} 
+}
 else if (n > 0)
 {
 printf("%d is positive\n", n);
-} 
+}
 else
 {
 printf("%d is zero\n", n);
