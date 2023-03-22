8c42f35eb77bb7152319ece84ea8f18cb84c71a8
Hossam Mahmoud <nhem318@gmail.com>
2023-03-22 12:09:13 +0200

    add if statement

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,12 +4,10 @@
 /* betty style doc for function main goes there */
 /**
  * _islower function checks if the letter is uppercase or lowercase
- * 
  * if the letter is lowercase -> reutrns 1
  * if the letter is uppercase -> reutrns 0
  * 
  */
-
 int _islower(int c)
 {
 
@@ -18,13 +16,13 @@ char letter = (char)c;
 if (isupper(letter))
 {
 
-return 0;
+return (0);
 
 }
 else
 {
 
-return 1;
+return (1);
 
 }
 
