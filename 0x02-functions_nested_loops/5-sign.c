05aa569691994bb0c897d1ab167dbe1e9a35b06a
Hossam Mahmoud <nhem318@gmail.com>
2023-03-22 14:15:01 +0200

    modify sign.c file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,20 +8,20 @@
  */
 int print_sign(int n)
 {
-    
-    if (n > 0)
-    {
-        return (1);
-        putchar('+');
-    }
-    else if (n == 0)
-    {
-        return (0);
-        putchar('0');
-    }else
-    {
-        return (-1);
-        putchar('-');
-    }
+
+if (n > 0)
+{
+putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+putchar('0');
+return (0);
+}else
+{
+putchar('-');
+return (-1);
+}
 
 }
