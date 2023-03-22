a9839eb7530c69c64d5282382f11f957f7702207
Hossam Mahmoud <nhem318@gmail.com>
2023-03-22 17:18:43 +0200

    add print_to_98.c file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all the numbers from n to 98
+ *
+ * @n: the end of the print loop
+ * Return: void
+ */
+void print_to_98(int n)
+{
+if (n < 98)
+{
+for (int i = n; i <= 98;i++)
+{
+if(i == 98){
+printf("%d",i)
+}
+else
+{
+printf("%d, ",i);
+}
+}
+}
+else if (n > 98)
+{
+for (int i = n; i >= 98;i--)
+{
+if (i == 98)
+{
+printf("%d", i)
+}
+else
+{
+printf("%d, ", i);
+}
+}
+}
+else
+{
+printf("%d", 98);
+}
+}
