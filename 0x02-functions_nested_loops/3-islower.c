20437b8e61d0cbfb19f5c3dd4b49ce27648d0509
Hossam Mahmoud <nhem318@gmail.com>
2023-03-22 12:03:37 +0200

    add the islowercase file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/* betty style doc for function main goes there */
+/**
+ * _islower function checks if the letter is uppercase or lowercase
+ * 
+ * if the letter is lowercase -> reutrns 1
+ * if the letter is uppercase -> reutrns 0
+ * 
+ */
+
+int _islower(int c)
+{
+
+char letter = (char)c;
+
+if (isupper(letter))
+{
+
+return 0;
+
+}
+else
+{
+
+return 1;
+
+}
+
+}
