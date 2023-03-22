f56fd605765f3b2476eaa0b0f884ac78ca9b2e8f
Hossam Mahmoud <nhem318@gmail.com>
2023-03-22 17:04:27 +0200

    add add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * add - adds two integers and returns the result
+ *
+ * @x: the first parameter to be added to the second parameter
+ * @y: the seconde parameter to be added to the first parameter
+ * Return: the result of the sum of the two integers 
+ */
+int add(int x, int y)
+{
+int result = x + y;
+return (result);
+}
