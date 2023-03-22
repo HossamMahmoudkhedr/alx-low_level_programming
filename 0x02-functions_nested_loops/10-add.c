2ee018469401a2d65abac509d2ff32fb23f54f44
Hossam Mahmoud <nhem318@gmail.com>
2023-03-22 17:06:04 +0200

    remove an unnecessary space

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
  *
  * @x: the first parameter to be added to the second parameter
  * @y: the seconde parameter to be added to the first parameter
- * Return: the result of the sum of the two integers 
+ * Return: the result of the sum of the two integers
  */
 int add(int x, int y)
 {
