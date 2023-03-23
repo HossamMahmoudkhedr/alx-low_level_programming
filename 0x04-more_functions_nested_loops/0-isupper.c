48802452746738b8743a0cf5332149211c8d171e
Hossam Mahmoud <nhem318@gmail.com>
2023-03-24 00:12:03 +0200

    add the new directory

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _isupper - checks if the character is uppercase or not
+ *
+ * @c: is the character to be checked
+ * Return: 1 for uppercase letters and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
