481f80eec434b146526b7e5580e4bac53823e80b
Hossam Mahmoud <nhem318@gmail.com>
2023-03-23 17:45:42 +0200

    aadd the new file 0x03-debuggin'

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - test if the postive_or_negative function returns a right value with 0
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+
+    int i;
+    i = 0;
+
+    positive_or_negative(0);
+    return (0);
+
+}
