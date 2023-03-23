4c2e38baada005c3a34da1b5d3b22f03f1a5bc11
Hossam Mahmoud <nhem318@gmail.com>
2023-03-23 17:51:40 +0200

    modify 0-main.c file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,20 +1,16 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - test if the postive_or_negative function returns a right value with 0
- *
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
 int main(void)
 {
-
 int i;
 
 i = 0;
 positive_or_negative(i);
 
 return (0);
-
 }
