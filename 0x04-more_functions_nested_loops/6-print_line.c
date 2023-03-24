ec93a8465d7bb1701473b3e55fb9ea1a81004eed
Hossam Mahmoud <nhem318@gmail.com>
2023-03-25 00:02:25 +0200

    Add print_line.c file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ *
+ * @n: the number of lines to be drawn
+ * Return: void
+ */
+
+void print_line(int n)
+{
+    int i;
+    i = 1;
+
+    while (i <= n && i > 0)
+    {
+        putchar('_');
+        i++;
+    }
+    putchar('\n');
+}
