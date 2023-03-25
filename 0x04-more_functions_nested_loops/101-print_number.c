d8511e124f8c07beb242c413237cf46d73323ecb
Hossam Mahmoud <nhem318@gmail.com>
2023-03-25 17:40:19 +0200

    modify print_number.c file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -17,7 +17,7 @@ num = n;
 if (num < 0)
 {
 num *= -1;
-_putchar('-');
+putchar('-');
 }
 
 /* count up */
@@ -35,12 +35,12 @@ while (num >= 0)
 {
 if (m == 1)
 {
-_putchar(num % 10 + '0');
+putchar(num % 10 + '0');
 num = -1;
 }
 else
 {
-_putchar((num / m % 10) + '0');
+putchar((num / m % 10) + '0');
 m /= 10;
 }
 }
