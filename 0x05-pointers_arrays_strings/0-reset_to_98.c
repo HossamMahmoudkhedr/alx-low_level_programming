7afd33fc708dd8323e17d025a0772a5b32e86d1c
Hossam Mahmoud <nhem318@gmail.com>
2023-03-27 17:47:48 +0200

    Adding a new directory

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset the value of the pointer to 98
+ *
+ * @n: The pointer which its value would be 98
+ * Return: 98 which is the new value of the pointer n
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+return (*n);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * _putchar - returns one character
+ * 
+ * @c: the character to be printed
+ * Return 1 on success 
+ * on error, -1 is returned
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
+
+void reset_to_98(int *n);
