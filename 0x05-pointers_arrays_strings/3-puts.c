3fe8475e6cefe2996d22be7990c06812ac1944f8
Hossam Mahmoud <nhem318@gmail.com>
2023-03-28 00:38:24 +0200

    add puts.c file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _puts - print a string
+ *
+ * @str: the string to be printed
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+putchar('\n');
+}
