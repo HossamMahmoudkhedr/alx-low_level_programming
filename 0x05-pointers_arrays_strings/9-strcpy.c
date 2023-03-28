1819f5c9b4bb88dc21e2e16193e9cf7600cd498a
Hossam Mahmoud <nhem318@gmail.com>
2023-03-28 05:49:17 +0200

    modify strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,7 +6,7 @@
  *
  * @dest: the pointer which would have the copy of src
  * @src: the pointer which has the original value
- * @Return:  the pointer to dest
+ * @Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
@@ -20,6 +20,6 @@ while (i < length)
 dest[i] = src[i];
 i++;
 }
-
+dest[i] = '\0';
 return (dest);
 }
