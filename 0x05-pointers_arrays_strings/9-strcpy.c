de5dcf50f3fb54153ba320ebea9ca6d504e4a69a
Hossam Mahmoud <nhem318@gmail.com>
2023-03-28 04:36:14 +0200

    add strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * @*_strcpy - copies the string pointed to by src
+ *
+ * @dest: the pointer which would have the copy of src
+ * @src: the pointer which has the original value
+ * @Return:  the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+int length;
+
+i = 0;
+length = strlen(src);
+while (i < length)
+{
+dest[i] = src[i];
+i++;
+}
+
+return (dest);
+}
