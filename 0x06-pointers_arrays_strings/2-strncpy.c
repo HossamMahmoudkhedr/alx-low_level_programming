ca93faf53fa2fcfcd61c260d5daad1a957f21758
Hossam Mahmoud <nhem318@gmail.com>
2023-03-30 01:28:12 +0200

    modify strncpy.c file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,21 +9,22 @@
  * @n: This is the number of bytes to copy
  *
  *Return: Copied string with the number of bytes
- */
+*/
 
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 
-for (i = 0; src[i] != '\0';i++)
+i = 0;
+while (i < n && *(src + i))
 {
-dest[i] = src[i];
+*(dest + i) = *(src + i);
+i++;
 }
 while (i < n)
 {
-dest[i] = '\0';
+*(dest + i) = '\0';
 i++;
 }
-
 return (dest);
 }
