337d8c3eccc2d1e546fd8a9780c168a585635b6f
Hossam Mahmoud <nhem318@gmail.com>
2023-03-31 00:16:37 +0200

    modify rev_array.c file

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,13 +9,12 @@
  */
 void reverse_array(int *a, int n)
 {
-int i, tmp;
+int i, c;
 
-for (i = 0; i < n - 2; i++)
+for (i = 0; (i < (n - 1) / 2); i++)
 {
-tmp = a[i];
-a[i] = a[n - 1];
-a[n - 1] = tmp;
-n--;
+c = a[i];
+a[i] = a[n - 1 - i];
+a[n - 1 - i] = c;
 }
 }
