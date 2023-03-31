4399d32039a49e14dd0d4644d94ce0ec7352e672
Hossam Mahmoud <nhem318@gmail.com>
2023-03-31 02:10:15 +0200

    modify cap_string.c file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ if (str[0] >= 97 && str[0] <= 122)
 {
 str[0] = str[0] - 32;
 }
-for(i = 1; str[i] != '\0'; i++)
+for (i = 1; str[i] != '\0'; i++)
 {
 if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ':' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
 {
