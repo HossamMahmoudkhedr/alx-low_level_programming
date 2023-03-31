72cf5db2e2ca793e6470d35c2db05342a4957b06
Hossam Mahmoud <nhem318@gmail.com>
2023-03-31 04:30:00 +0200

    modify print_number.c file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * print_number -print number putchar
  * @n:integer
  * Return: void
  */
+
+
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
 void print_number(int n)
 {
 int x = n;
 
 if (n < 0)
 {
-putchar('-');
+_putchar('-');
 x = -x;
 }
 if ((x / 10) > 0)
@@ -19,5 +26,5 @@ if ((x / 10) > 0)
 print_number(x / 10);
 }
 
-putchar(x % 10 + '0');
+_putchar(x % 10 + '0');
 }
