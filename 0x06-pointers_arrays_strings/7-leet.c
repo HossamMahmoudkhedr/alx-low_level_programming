99d325f8cb225c5a2bec2d92cd3dab6f4e092ec4
Hossam Mahmoud <nhem318@gmail.com>
2023-03-31 02:44:57 +0200

    add 7-leet.c file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * leet - encodes a string into 1337
+ *
+ * @str: the pointer to string
+ * Return: the string after changing the letters
+ */
+
+char *leet(char *str)
+{
+int i,j;
+char letters[] = "AEOTLaeotl";
+char nums[] = "4307143071";
+
+for (i = 0;str[i] != '\0';i++)
+{
+for (j = 0; letters[j] != '\0';j++)
+{
+if (str[i] == letters[j])
+{
+str[i] = nums[j];
+}
+}
+}
+
+return (str);
+}
