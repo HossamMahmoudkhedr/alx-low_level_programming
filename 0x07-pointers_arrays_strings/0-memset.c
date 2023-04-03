d4f713a6e54ce07b0e35f250487ea51336e17592
Hossam Mahmoud <nhem318@gmail.com>
2023-04-04 01:49:47 +0200

    add memset.c file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _memset -  fills memory with a constant byte
+ *
+ * @s: pointer to a string
+ * @b: constant byte
+ * @n: number of bytes
+ * Return: the string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+while (n--)
+{
+*s++ = (unsigned char)b;
+}
+return (s);
+}
