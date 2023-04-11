57dc60409744f6d9e6254fa4049e7e44896ba31a
Hossam Mahmoud <nhem318@gmail.com>
2023-04-11 13:43:27 +0200

    modify create-array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars, and initializes it with a specific char
+ * create_array - creates an array of chars
  *
  * @size: the size of bytes needed to be allocated in memeory
  * @c: the character to allocated in the saved blocks
