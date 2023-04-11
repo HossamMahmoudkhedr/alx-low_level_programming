d56181980e6b2dc585531ed3834cdbd4f9fc33f4
Hossam Mahmoud <nhem318@gmail.com>
2023-04-11 13:37:03 +0200

    adding new directory

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and initializes it with a specific char
+ *
+ * @size: the size of bytes needed to be allocated in memeory
+ * @c: the character to allocated in the saved blocks
+ * Return: NULL if size = 0, pointer to the array, or NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+if (size == 0)
+{
+return (NULL);
+}
+
+char *arr = (int *)malloc(sizeof(char) * size);
+if (arr == NULL)
+{
+return (NULL);
+}
+
+for (unsigned int i = 0; i < size; i++)
+{
+arr[i] = c;
+}
+return (arr);
+}
