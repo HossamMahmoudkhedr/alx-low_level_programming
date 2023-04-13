64a81d3289d8cc9ba1bdad1b835b6562840d6d27
Hossam Mahmoud <nhem318@gmail.com>
2023-04-13 18:23:43 +0200

    modify string_nconcat.c file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,18 +13,21 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+char *result;
+size_t len1;
+size_t len2;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
 
-size_t len1 = strlen(s1);
-size_t len2 = strlen(s2);
+len1 = strlen(s1);
+len2 = strlen(s2);
 
 if (n >= len2)
 n = len2;
 
-char *result = malloc(len1 + n + 1);
+result = malloc(len1 + n + 1);
 if (result == NULL)
 return NULL;
 
@@ -32,5 +35,5 @@ memcpy(result, s1, len1);
 memcpy(result + len1, s2, n);
 result[len1 + n] = '\0';
 
-return result;
+return (result);
 }
