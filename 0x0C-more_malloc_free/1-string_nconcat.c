92121dd4f6ba0b88dbbf0394536d36e88862c9b6
Hossam Mahmoud <nhem318@gmail.com>
2023-04-13 18:22:05 +0200

    modify string_nconcat.c file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * string_nconcat - concatenates two strings
@@ -9,51 +10,27 @@
  * @n: number of bytes of s2
  * Return: pointer should point to a newly allocated space in memory
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-int len1, len2, i, j, newSize;
-char *newStr;
-len1 = 0;
-len2 = 0;
-
 if (s1 == NULL)
-{
 s1 = "";
-}
 if (s2 == NULL)
-{
 s2 = "";
-}
-while (s1[len1] != '\0')
-{
-len1++;
-}
 
-while (s2[len2] != '\0')
-{
-len2++;
-}
-if (n < len2)
-{
-len2 = n;
-}
-newSize = len1 + len2;
-newStr = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-if (newStr == NULL)
-{
-return (NULL);
-}
-for (i = 0; i < len1; i++)
-{
-newStr[i] = s1[i];
-}
-len2 = i;
-len1 = 0;
-for (j = len2; j < newSize; j++)
-{
-newStr[j] = s2[len1];
-len1++;
-}
-newStr[j] = '\0';
-return (newStr);
+size_t len1 = strlen(s1);
+size_t len2 = strlen(s2);
+
+if (n >= len2)
+n = len2;
+
+char *result = malloc(len1 + n + 1);
+if (result == NULL)
+return NULL;
+
+memcpy(result, s1, len1);
+memcpy(result + len1, s2, n);
+result[len1 + n] = '\0';
+
+return result;
 }
