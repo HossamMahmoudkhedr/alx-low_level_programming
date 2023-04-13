679cbb1d40f10082e7ee55dc1fbb571712029f9e
Hossam Mahmoud <nhem318@gmail.com>
2023-04-13 18:18:38 +0200

    modify string_nconcat.c file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,49 +11,49 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    int len1, len2, i, j, newSize;
-    char *newStr;
-    len1 = 0;
-    len2 = 0;
+int len1, len2, i, j, newSize;
+char *newStr;
+len1 = 0;
+len2 = 0;
 
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
-    while (s1[len1] != '\0')
-    {
-        len1++;
-    }
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+while (s1[len1] != '\0')
+{
+len1++;
+}
 
-    while (s2[len2] != '\0')
-    {
-        len2++;
-    }
-    if(n < len2)
-    {
-        len2 = n;
-    }
-    newSize = len1 + len2;
-    newStr = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
-    if (newStr == NULL)
-    {
-        return (NULL);
-    }
-    for (i = 0; i < len1; i++)
-    {
-        newStr[i] = s1[i];
-    }
-    len2 = i;
-    len1 = 0;
-    for (j = len2; j < newSize; j++)
-    {
-        newStr[j] = s2[len1];
-        len1++;
-    }
-    newStr[j] = '\0';
-    return (newStr);
+while (s2[len2] != '\0')
+{
+len2++;
+}
+if (n < len2)
+{
+len2 = n;
+}
+newSize = len1 + len2;
+newStr = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+if (newStr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < len1; i++)
+{
+newStr[i] = s1[i];
+}
+len2 = i;
+len1 = 0;
+for (j = len2; j < newSize; j++)
+{
+newStr[j] = s2[len1];
+len1++;
+}
+newStr[j] = '\0';
+return (newStr);
 }
