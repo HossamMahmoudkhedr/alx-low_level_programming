cf5a953f55c2d03b18d6bd9f7a524e069e6dd45a
Hossam Mahmoud <nhem318@gmail.com>
2023-04-13 23:47:54 +0200

    Modify calloc.c file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,14 +5,15 @@
  * _calloc - allocates memory for an array, using malloc
  *
  * @nmemb: array of elements
- * @size: size of the elemenst
+ * @size: size of the elements
  * Return: If nmemb or size is 0 returns NULL if malloc fails returns NULL
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 int *ptr;
-int j, i;
+int i, j;
+
 if (nmemb == 0 || size == 0)
 {
 return (NULL);
@@ -23,12 +24,14 @@ if (ptr == NULL)
 {
 return (NULL);
 }
+
 for (i = 0; i < nmemb; i++)
 {
-for (j = 0; j < size;j++)
+for (j = 0; j < size; j++)
 {
-ptr[j] = 0;
+ptr[i * size + j] = 0;
 }
 }
 
+return ptr;
 }
