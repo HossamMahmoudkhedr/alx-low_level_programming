538b8abc7860c919a907b208ca8b6c68e59a807c
Hossam Mahmoud <nhem318@gmail.com>
2023-04-14 11:29:35 +0200

    modify array_range.c file

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,17 +19,13 @@ return (NULL);
 }
 size = (max - min) + 1;
 arr = malloc(size * sizeof(int));
-
 if (arr == NULL)
 {
 return (NULL);
 }
-
 for (i = 0; i < size; i++)
 {
 arr[i] = min + i;
-
 }
-
 return (arr);
 }
