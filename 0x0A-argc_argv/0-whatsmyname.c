7080527bc213abae40f791abf37889fc80132f08
Hossam Mahmoud <nhem318@gmail.com>
2023-04-15 14:03:29 +0200

    modify whatsmyname.c file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+/**
+ * main - prints its name, followed by a new line
+ *
+ * @argc: number of strings
+ * @argv: array of strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
 {
-    printf("%s", argv[0]);
-    return (0);
+printf("%s\n", argv[0]);
+return (0);
 }
