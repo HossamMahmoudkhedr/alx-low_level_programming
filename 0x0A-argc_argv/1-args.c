3bac7b18ccb02c4ddfd2168c9ffedba99b287325
Hossam Mahmoud <nhem318@gmail.com>
2023-04-15 14:53:27 +0200

    modify args.c file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,10 +5,10 @@
  *
  * @args: the number fo arguments
  * @argv: array of strings
- * Return: 0
+ * Return: Always 0 (Success)
  */
 
-int main(int argc, char argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 printf("%d\n", argc - 1);
 return (0);
