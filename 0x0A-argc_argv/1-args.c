6a4d8036606bc7d29a4d41e51b61070dc40de64c
Hossam Mahmoud <nhem318@gmail.com>
2023-04-15 14:43:07 +0200

    modify args.c file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,8 +8,8 @@
  * Return: 0
  */
 
-int main(int args, char* argv[] __attribute__((unused)))
+int main(int args, char **argv[] __attribute__((unused)))
 {
-printf("%d\n", args);
+printf("%d\n", args - 1);
 return (0);
 }
