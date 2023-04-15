d8272a5f880cff472da1407ef1f476d87d93c0dd
Hossam Mahmoud <nhem318@gmail.com>
2023-04-15 14:39:11 +0200

    add args.c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the number of arguments passed into it
+ *
+ * @args: the number fo arguments
+ * @argv: array of strings
+ * Return: 0
+ */
+
+int main(int args, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", args);
+return (0);
+}
