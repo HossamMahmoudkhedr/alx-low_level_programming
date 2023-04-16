a2c07710b756e1f91c355d988f63976fa3efac86
Hossam Mahmoud <nhem318@gmail.com>
2023-04-16 13:01:34 +0200

    add new directory and the first file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion -  prints a string, followed by a new line
+ *
+ * @s: the string to be printed
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+putchar(*s);
+*s++;
+if (*s != '\0')
+{
+_puts_recursion(s);
+}
+else
+{
+putchar('\n');
+}
+}
