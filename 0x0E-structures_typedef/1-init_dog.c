7734e03904cdef5fe3a5b950492b1c579dd6e9bd
Hossam Mahmoud <nhem318@gmail.com>
2023-04-17 23:36:52 +0200

    modify init_dog.c file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,9 +8,7 @@
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the owner of the dog
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
