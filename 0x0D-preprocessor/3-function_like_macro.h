aab2814c389f4b2974e3e001631388cf78b7e903
Hossam Mahmoud <nhem318@gmail.com>
2023-04-17 18:09:01 +0200

    modify function_like_macro.h file

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _FUNCTION_LIKE_MACRO
-#define _FUNCTION_LIKE_MACRO
-#define ABS(X) ((X) * (-1))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#define ABS(X) ((X) < 0 ? -(X) : (X))
 #endif /* _FUNCTION_LIKE_MACRO_H */
