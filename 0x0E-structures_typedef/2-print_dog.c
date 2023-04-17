faa33786e83ba5b3fb43ee742bc1e2094069cb2c
Hossam Mahmoud <nhem318@gmail.com>
2023-04-18 00:03:56 +0200

    modify print_dog function

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,6 +12,28 @@ if (d != 0)
 {
 if (d->name == 0)
 {
+    printf("Name: (nil)\n");
+}
+else
+{
+    printf("Name: %s\n", d->name);
+}
+printf("Age: %f\n", d->age);
+if (d->owner == 0)
+{
+    printf("Owner: (nil)\n");
+}
+else
+{
+    printf("Owner: %s\n", d->owner);
+}
+
+}
+}
+
+/*
+if (d->name == 0)
+{
 printf("Name: (nil)\n");
 printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner);
@@ -28,5 +50,4 @@ printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner);
 }
-}
-}
+*/
