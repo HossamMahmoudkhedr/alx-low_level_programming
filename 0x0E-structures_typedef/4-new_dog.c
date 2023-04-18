d77dfbca9b8d0d06f92a785d70fb515d19dfe42a
Hossam Mahmoud <nhem318@gmail.com>
2023-04-18 02:26:16 +0200

    modify new_dog function

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,37 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 
-/**
- * _strdup - returns a pointer to a newly allocated space in memory, which
- * contains a copy of the string given as a parameter.
- * @str: string to copy
- *
- * Return: Pointer
- */
-char *_strdup(char *str)
-{
-int l, i;
-char *s;
-
-if (str == NULL)
-return (0);
-
-l = 0;
-while (*(str + l))
-l++;
-
-s = malloc(sizeof(char) * l + 1);
-
-if (s == 0)
-return (0);
-
-for (i = 0; i <= l; i++)
-{
-*(s + i) = *(str + i);
-}
-return (s);
-}
 /**
  * new_dog -  creates a new dog
  *
@@ -49,8 +20,8 @@ if (newDog == NULL)
 {
 return (NULL);
 }
-newDog->name = _strdup(name);
-newDog->owner = _strdup(owner);
+newDog->name = strdup(name);
+newDog->owner = strdup(owner);
 
 newDog->name = name;
 newDog->age = age;
