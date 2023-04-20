8a86c969a7c5a9cd40e1e4ae7c99a8b3db8f72ce
Hossam Mahmoud <nhem318@gmail.com>
2023-04-20 23:40:59 +0200

    add print_all.c fiel

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+/**
+ * print_all - prints anything
+ *
+ * @format: list of types of arguments passed to the function
+ * @...: ellipsis
+ * Return: void
+ */
+
+void print_all(const char *const format, ...)
+{
+char c;
+int i;
+double f;
+char *str;
+va_list arg;
+i = 0;
+va_start(arg, format);
+while (format && format[i])
+{
+switch (format[i++])
+{
+case 'c':
+c = va_arg(arg, int);
+printf("%c", c);
+break;
+case 'i':
+i = va_arg(arg, int);
+printf("%d", i);
+break;
+case 'f':
+f = va_arg(arg, double);
+printf("%f", f);
+break;
+case 's':
+str = va_arg(arg, char *);
+if (str)
+{
+printf("%s", str);
+}
+else
+{
+printf("(nil)");
+}
+break;
+default:
+continue;
+}
+if (format[i])
+{
+printf(", ");
+}
+}
+va_end(arg);
+printf("\n");
+}
