d802a434e2181b7c4bfc6495b63b04da34b066be
Hossam Mahmoud <nhem318@gmail.com>
2023-04-27 15:36:27 +0200

    modify add_node_end.c file

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,24 +14,35 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 int length, i;
-list_t *newNode;
+list_t *newNode, *temp;
 newNode = malloc(sizeof(list_t));
 if (newNode == NULL)
 {
 return (NULL);
 }
-length = 0;
-newNode->str = strdup(str);
-newNode->next = *head;
 i = 0;
 while (str[i] != '\0')
 {
 length++;
 i++;
 }
-
-newNode->len = length;
-*head = newNode;
+length = 0;
+newNode->str = strdup(str);
 newNode->next = NULL;
+newNode->len = length;
+temp = *head;
+
+if (temp == NULL)
+{
+*head = newNode;
+}
+else
+{
+while (temp->next != NULL)
+{
+temp = temp->next;
+temp->next = newNode;
+}
+}
 return (*head);
 }
