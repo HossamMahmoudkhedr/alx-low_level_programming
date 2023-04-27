2b62affb9a800da221c1c791193ea33919bb3108
Hossam Mahmoud <nhem318@gmail.com>
2023-04-27 15:47:38 +0200

    modify free_list.c file

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,10 +6,11 @@
  * free_list - frees a list_t list.
  *
  * @head: the head of the linked list
+ * Return: void
  */
 void free_list(list_t *head)
 {
-list_t current;
+list_t *current;
 while (head != NULL)
 {
 current = head;
