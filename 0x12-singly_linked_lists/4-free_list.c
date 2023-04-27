24bde3b76558d6482e27f92fc44a28e52c74b6d7
Hossam Mahmoud <nhem318@gmail.com>
2023-04-27 15:46:19 +0200

    add free_list.c file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_list - frees a list_t list.
+ *
+ * @head: the head of the linked list
+ */
+void free_list(list_t *head)
+{
+list_t current;
+while (head != NULL)
+{
+current = head;
+head = head->next;
+free(current);
+}
+}
