8134550bb1f1b08b2eeb9bb87210bde0db6ff16c
Hossam Mahmoud <nhem318@gmail.com>
2023-05-18 23:50:19 +0200

    add print_binary.c file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <limits.h>
+
+/**
+ * print_binary - 
+ *
+ * @n: the number
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+unsigned long int bit = 1ul << 63;
+char character = '0';
+
+while (!(bit & n) && bit != 0)
+{
+bit = bit >> 1;
+}
+
+if (bit == 0)
+{
+write(1, &c, 1);
+}
+
+while (bit)
+{
+if (bit & n)
+{
+character = '1';
+}
+else
+{
+character = '0';
+}
+write(1, &character, 1);
+bit = bit >> 1;
+}
+
+}
