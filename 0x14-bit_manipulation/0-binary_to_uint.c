6f2dd4bd5c200a3637fed7132beb54935261ddf0
Hossam Mahmoud <nhem318@gmail.com>
2023-05-18 23:37:52 +0200

    add new directory with binary_to_unit.c file

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include <stddef.h>
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ *
+ * @b: the string to be converted
+ * Return: the unsigned int value
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int number;
+
+if (b == NULL)
+{
+return (0);
+}
+
+for (n = 0; *b != 0; b++)
+{
+if (*b == '0')
+{
+number = number << 1;
+}
+else if (*b == '1')
+{
+number = number << 1;
+number++;
+}
+else
+{
+return (0);
+}
+}
+return (n);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef BIT_MANIPULATION
+#define BIT_MANIPULATION
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
