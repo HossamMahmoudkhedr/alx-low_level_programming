90fff1fe7015cfe90fbabe35bf031d1e51805a85
Hossam Mahmoud <nhem318@gmail.com>
2023-06-16 17:19:33 +0200

    add some comments

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * dlistint_len - returns the length of the linked list
+ *
+ * @h: a pointer to the first node in the linked list
+ * Return: the length of the linked list
+ */
+
 size_t dlistint_len(const dlistint_t *h)
 {
     size_t length = 0;
