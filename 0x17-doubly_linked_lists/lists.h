667cf780d836b92195488984793ec9c419153018
Hossam Mahmoud <nhem318@gmail.com>
2023-06-16 15:59:05 +0200

    modify lists.h file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,8 +1,6 @@
 #ifndef DOUBLY_LINKED_LISTS
 #define DOUBLY_LINKED_LISTS
 
-size_t print_dlistint(const dlistint_t *h);
-
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -18,4 +16,6 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
 #endif
