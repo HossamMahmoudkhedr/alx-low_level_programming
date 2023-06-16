fc01b80b51b06ea52eeb91e9fc44c1038bec47b8
Hossam Mahmoud <nhem318@gmail.com>
2023-06-16 17:44:03 +0200

    modify add_dnodeint.c file

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,6 +3,27 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - returns the length of the linked list
+ *
+ * @h: a pointer to the first node in the linked list
+ * Return: the length of the linked list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t length = 0;
+
+while (h != NULL)
+{
+length++;
+h = h->next;
+}
+
+return (length);
+}
+
+
+/**
  * add_dnodeint - add a new node to the beginning of the list
  *
  * @head: a pointer to the first node
