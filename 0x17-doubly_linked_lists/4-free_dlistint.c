b16bdb35a56b7a19457f9265a5051337d904d06d
Hossam Mahmoud <nhem318@gmail.com>
2023-06-17 12:28:32 +0200

    modify free_dlistint.c file

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -14,6 +14,11 @@ void free_dlistint(dlistint_t *head)
     dlistint_t *temp;
 
     temp = head;
+    while (head->prev != NULL)
+    {
+		head = head->prev;
+    }
+    
     while (head != NULL)
     {
         head = head->next;
