be7ca8d85c80b8eb2dd8b5ffb91365d96b89eb0a
HossamMahmoudKhedr <nhem318@gmail.com>
2023-06-20 07:17:36 +0200

    modify the dynamic library

--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -1,84 +1,255 @@
 #include "main.h"
-
+/**
+ * _putchar - it is a function prints a character
+ *
+ * @c: the char to be printed
+ * Return: return an integer
+ */
 int _putchar(char c)
 {
 
 }
 
+/**
+ * _islower - check if a character is lowercase
+ *
+ * @c: the number of the character
+ * Return: it returns an integer
+ */
 int _islower(int c)
 {
 
 }
 
+/**
+ * _isalpha - check if the character is alphabet
+ *
+ * @c: the charachter to be checked
+ * Return: it returns and integer
+ */
 int _isalpha(int c)
 {
 
 }
+
+/**
+ * _abs - get the absolute value of a number
+ *
+ * @n: the number
+ * Return: return an integer
+ */
 int _abs(int n)
 {
 
 }
+
+/**
+ * _isupper - check if the character is uppercase
+ *
+ * @c: the character to be checked
+ * Return: an integer
+ */
+
 int _isupper(int c)
 {
 
 }
+
+/**
+ * _isdigit - check if the number is digit
+ *
+ * @c: the number to be checked
+ * Return: return an integer
+ */
+
 int _isdigit(int c)
 {
 
 }
+
+/**
+ * _strlen - get the length of a string
+ *
+ * @s: a pointer to the first character
+ * Return: the length of the string
+ */
+
 int _strlen(char *s)
 {
 
 }
+
+/**
+ * _puts - it prints a string
+ *
+ * @s: a pointer to the first character 
+ * Return: void
+ */
 void _puts(char *s)
 {
 
 }
+
+/**
+ * _strcpy - make a copy of a string
+ *
+ * @dest: the destination to copy the string
+ * @src: the string to be copied
+ * Return: it returns a char
+ */
+
 char *_strcpy(char *dest, char *src)
 {
 
 }
+/**
+ * _atoi - Converts a string to an integer.
+ *
+ * @s: The string to convert.
+ *
+ * Return: The converted integer.
+ */
 int _atoi(char *s)
 {
-
+  /* Function implementation goes here */
 }
+
+/**
+ * _strcat - Concatenates two strings.
+ *
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: A pointer to the resulting string.
+ */
 char *_strcat(char *dest, char *src)
 {
-
+  /* Function implementation goes here */
 }
+
+/**
+ * _strncat - Concatenates two strings up to n bytes.
+ *
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of bytes to concatenate.
+ *
+ * Return: A pointer to the resulting string.
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-
+  /* Function implementation goes here */
 }
+
+/**
+ * _strncpy - Copies a string up to n bytes.
+ *
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copy.
+ *
+ * Return: A pointer to the resulting string.
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-
+  /* Function implementation goes here */
 }
+
+/**
+ * _strcmp - Compares two strings.
+ *
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: An integer less than, equal to, or greater than zero if s1 is found,
+ * respectively, to be less than, to match, or be greater than s2.
+ */
 int _strcmp(char *s1, char *s2)
 {
-
+  /* Function implementation goes here */
 }
+
+/**
+ * _memset - Fills a block of memory with a specific value.
+ *
+ * @s: Pointer to the memory area to fill.
+ * @b: Value to be set.
+ * @n: Number of bytes to be set to the value.
+ *
+ * Return: A pointer to the memory area s.
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-
+  /* Function implementation goes here */
 }
+
+/**
+ * _memcpy - Copies n bytes from memory area src to memory area dest.
+ *
+ * @dest: Pointer to the destination memory area.
+ * @src: Pointer to the source memory area.
+ * @n: Number of bytes to copy.
+ *
+ * Return: A pointer to the destination memory area.
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-
+  /* Function implementation goes here */
 }
+
+/**
+ * _strchr - Locates the first occurrence of a character in a string.
+ *
+ * @s: The string to search in.
+ * @c: The character to locate.
+ *
+ * Return: A pointer to the first occurrence of the character c in the string s,
+ * or NULL if the character is not found.
+ */
 char *_strchr(char *s, char c)
 {
-
+  /* Function implementation goes here */
 }
+
+/**
+ * _strspn - Calculates the length of the initial segment of a string
+ * consisting only of characters from another string.
+ *
+ * @s: The string to be scanned.
+ * @accept: The string containing the characters to match.
+ *
+ * Return: The number of characters in the initial segment of s that consist
+ * only of characters from accept.
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-
+  /* Function implementation goes here */
 }
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ *
+ * @s: The string to be searched.
+ * @accept: The string containing the characters to search for.
+ *
+ * Return: A pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found.
+ */
 char *_strpbrk(char *s, char *accept)
 {
-
+  /* Function implementation goes here */
 }
+
+/**
+ * _strstr - Locates a substring in a string.
+ *
+ * @haystack: The string to search in.
+ * @needle: The substring to find.
+ *
+ * Return: A pointer to the beginning of the located substring, or NULL if the
+ * substring is not found.
+ */
 char *_strstr(char *haystack, char *needle)
 {
-
+  /* Function implementation goes here */
 }
