7c0547ccc031bb98ca29c43875ef37673f326feb
HossamMahmoudKhedr <nhem318@gmail.com>
2023-07-08 14:13:43 +0200

    modify djb2.c file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,7 +3,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table.
+ * hash_table_create - creates a hash table
  *
  * @size: the size of the table
  * Return: pointer to the newly created hash table, or NULL otherwise
--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -4,8 +4,8 @@
 
 /**
  * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
  *
+ * @str: string used to generate hash value
  * Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
