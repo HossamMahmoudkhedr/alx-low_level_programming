0bd196effe1ad9243c32be7245b82034a3901fa0
HossamMahmoudKhedr <nhem318@gmail.com>
2023-07-08 11:11:55 +0200

    modify hash_table_create.c file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,22 +11,26 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table;
-    unsigned long int i;
-    table = malloc(sizeof(hash_table_t));
-    if (table == NULL)
-    {
-        return (NULL);
-    }
+hash_table_t *table;
+unsigned long int i;
+table = malloc(sizeof(hash_table_t));
+if (table == NULL)
+{
+return (NULL);
+}
 
-    table->size = size;
+table->size = size;
 
-    table->array = (hash_node_t**) malloc(sizeof(hash_node_t) * table->size);
+table->array = malloc(sizeof(hash_node_t) * table->size);
 
-    for (i = 0; i < table->size; i++)
-    {
-        table->array[i] = NULL;
-    }
+if (table->array == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < table->size; i++)
+{
+table->array[i] = NULL;
+}
 
-    return (table);
+return (table);
 }
