bd44c1e9eb44bdb7b8acb90a848c6e8d4fa41b48
HossamMahmoudKhedr <nhem318@gmail.com>
2023-07-09 09:07:40 +0200

    modify hash_table_set.c file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,16 +3,18 @@
 #include <string.h>
 #include "hash_tables.h"
 
-hash_node_t create_item(char *key, char *value)
+hash_node_t *create_item(const char *key, const char *value)
 {
 hash_node_t *item = malloc(sizeof(hash_node_t));
 if (item == NULL)
 {
 return (NULL);
 }
+item->key = (char *)malloc(strlen(key) + 1);
+item->value = (char *)malloc(strlen(value) + 1);
+strcpy((char *)item->key, key);
+strcpy((char *)item->value, value);
 
-strcpy(item->key, key);
-strcpy(item->value, value);
 return (item);
 }
 
@@ -20,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 hash_node_t *item = create_item(key, value);
 
-unsigned long int index = key_index(key, ht->size);
+unsigned long int index = key_index((const unsigned char *)key, ht->size);
 
 hash_node_t *current_item = ht->array[index];
 
@@ -34,4 +36,5 @@ return (0);
 ht->array[index] = item;
 return (1);
 }
+return (0);
 }
