39f97dd13fa98c5a69ed9b2b1354e58f83ae9714
HossamMahmoudKhedr <nhem318@gmail.com>
2023-07-09 09:14:11 +0200

    modify hash_table_set.c file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include "hash_tables.h"
 
+/**
+ * create_item - Create an item object to be added
+ *
+ * @key: the key
+ * @value: the value
+ * Return: the new item
+ */
+
 hash_node_t *create_item(const char *key, const char *value)
 {
 hash_node_t *item = malloc(sizeof(hash_node_t));
@@ -18,6 +26,15 @@ strcpy((char *)item->value, value);
 return (item);
 }
 
+/**
+ * hash_table_set - insert a new item in the table
+ *
+ * @ht: the hash table
+ * @key: the key of the item
+ * @value: the value of the item
+ * Return: 1 if succeeded, 0 otherwise 
+ */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 hash_node_t *item = create_item(key, value);
@@ -32,7 +49,6 @@ if (index >= ht->size)
 {
 return (0);
 }
-
 ht->array[index] = item;
 return (1);
 }
