3cebe630dbde05495f1a8b4dc2e8fe75d27ec939
HossamMahmoudKhedr <nhem318@gmail.com>
2023-07-09 08:52:51 +0200

    modify hash_table_set.c file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,33 +5,33 @@
 
 hash_node_t create_item(char *key, char *value)
 {
-    hash_node_t *item = malloc(sizeof(hash_node_t));
-    if (item == NULL)
-    {
-        return (NULL);
-    }
+hash_node_t *item = malloc(sizeof(hash_node_t));
+if (item == NULL)
+{
+return (NULL);
+}
 
-    strcpy(item->key, key);
-    strcpy(item->value, value);
-    return (item);
+strcpy(item->key, key);
+strcpy(item->value, value);
+return (item);
 }
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *item = create_item(key, value);
+hash_node_t *item = create_item(key, value);
 
-    unsigned long int index = key_index(key, ht->size);
+unsigned long int index = key_index(key, ht->size);
 
-    hash_node_t *current_item = ht->array[index];
+hash_node_t *current_item = ht->array[index];
 
-    if (current_item == NULL)
-    {
-        if (index >= ht->size)
-        {
-            return (0);
-        }
+if (current_item == NULL)
+{
+if (index >= ht->size)
+{
+return (0);
+}
 
-        ht->array[index] = item;
-        return (1);
-    }
+ht->array[index] = item;
+return (1);
+}
 }
